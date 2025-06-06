e2d431d156a730c94ec0e7bd9208ba73f7379319
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-06 13:47:46 +0600

    upload project file and vue Methods, Directives, data binding, raw html

---
+++ section 2/app.js
@@ -0,0 +1,35 @@
+// vm = vue model
+
+const vm = Vue.createApp({
+    data(){
+        return {
+            firstName:'john',
+            lastName:'doe',
+            google_url:'https://google.com',
+            raw_url:'<a href="https://google.com" target="_blank" class="text-blue-500 hover:text-blue-600 font-medium underline coursor-pointer transition-colors" >Google</a>'
+        }
+    },
+    methods: {
+        fullName(){
+            const lastname=this.lastName
+            const finalname=lastname.charAt(0).toUpperCase()+lastname.slice(1)
+            return `${this.firstName} ${finalname}`
+            // return `${this.firstName} ${this.lastName.toUpperCase()}`
+        }
+    },
+
+}).mount('#app')
+
+// setTimeout(()=>{
+//     vm.firstName="Jhon",
+//     vm.lastName="Wick"
+// },2000)
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             firstName:"jewel",
+//             lastName:"rana"
+//         }
+//     }
+// }).mount('#second_app')
