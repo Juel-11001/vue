b093ae7837e49499158ec9bc38bbb0059ebdefe2
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-17 20:12:04 +0600

    quize app setting and rendering the questions

---
+++ quiz-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ctr">
+    <!--question-->
+    <Questions v-if="questionsAnswered < questions.length" :questions="questions"  />
+    <!--result -->
+    <Result v-else/>
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
+</template>
+
+<script>
+import Questions from "@/components/Questions.vue";
+import Result from "@/components/Result.vue";
+import {questions, results} from "@/data/quizeData.js";
+
+export default {
+  name:"App",
+  components:{
+    Questions,
+    Result
+  },
+  data(){
+    return {
+      questionsAnswered:0,
+      questions,
+      results
+    }
+  }
+}
+</script>
