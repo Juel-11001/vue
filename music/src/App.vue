82f3b59987b097d9acae987cbaaa4af124e0d857
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-22 00:05:36 +0600

    validation components

--- music/src/App.vue
+++ music/src/App.vue
@@ -10,10 +10,7 @@
       <div class="text-white main-header-content">
         <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
         <p class="w-full md:w-8/12 mx-auto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-          suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-          pulvinar, fringilla lorem eget, ullamcorper urna.
+          Enjoy high-quality music streaming crafted for every mood and moment. Whether you're building focus, relaxing after a long day, or getting pumped for the weekend, our handpicked playlists and artists deliver the perfect sound experience. Listen without limits—anytime, anywhere.
         </p>
       </div>
     </div>
