c24d9dea382910c46eba14d6bccba58534242036
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-24 14:43:03 +0600

    Storing the user data form into firestore in firebase

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -86,7 +86,7 @@
 </template>
 
 <script>
-import firebase from "@/includes/firebase.js";
+import {auth,usersCollection} from "@/includes/firebase.js";
 export default {
   name:"Register Form",
   data(){
@@ -117,7 +117,7 @@ export default {
       this.reg_alert_msg="Please wait! Your account is being created.";
       let userCred=null;
       try {
-       userCred= await firebase.auth().createUserWithEmailAndPassword(
+       userCred= await auth.createUserWithEmailAndPassword(
          values.email, values.password
        )
       }catch (error){
@@ -127,9 +127,23 @@ export default {
         // console.log(error)
         return;
       }
+      try{
+        await usersCollection.add({
+          name:values.name,
+          email:values.email,
+          age:values.age,
+          country:values.country,
+        });
+      }catch (error){
+        this.reg_in_submission=false;
+        this.reg_alert_variant="bg-red-500";
+        this.reg_alert_msg="An unexpected error occurred. Please try again later.";
+        console.log(error);
+        return;
+      }
       this.reg_alert_variant="bg-green-500";
       this.reg_alert_msg="Success! Your account has been created."
-      console.log(userCred);
+      // console.log(userCred);
     }
   }
 }
--- music/src/includes/firebase.js
+++ music/src/includes/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app"
-// import { initializeApp } from "firebase/app";
 import "firebase/auth"
+import "firebase/firestore"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCECER_igk7YV0VZIP-u5n9FyFi0XzrHuM",
@@ -13,4 +13,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const auth=firebase.auth();
+const db=firebase.firestore();
+const usersCollection=db.collection('users');
+export{
+  auth,
+  db,
+  usersCollection
+}
