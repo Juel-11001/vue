342a09c520394f424f0c65d5cc1db5b507db30b4
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-24 13:08:54 +0600

    Handling the response

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -115,12 +115,21 @@ export default {
       this.reg_in_submission=true;
       this.reg_alert_variant="bg-blue-500";
       this.reg_alert_msg="Please wait! Your account is being created.";
-       const  userCred= await firebase.auth().createUserWithEmailAndPassword(
+      let userCred=null;
+      try {
+       userCred= await firebase.auth().createUserWithEmailAndPassword(
          values.email, values.password
        )
+      }catch (error){
+        this.reg_in_submission=false;
+        this.reg_alert_variant="bg-red-500";
+        this.reg_alert_msg="An unexpected error occurred. Please try again later."
+        // console.log(error)
+        return;
+      }
       this.reg_alert_variant="bg-green-500";
       this.reg_alert_msg="Success! Your account has been created."
-
+      console.log(userCred);
     }
   }
 }
