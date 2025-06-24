779dd4908b9873a4fbfa827c07a80b258670c395
Juel-11001 <mdjewel20172017@gmail.com>
2025-06-24 19:25:37 +0600

    Login the user after singup and using actions

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -87,6 +87,9 @@
 
 <script>
 import {auth,usersCollection} from "@/includes/firebase.js";
+import {mapActions} from "pinia";
+import useUserStore from "@/stores/user.js"
+
 export default {
   name:"Register Form",
   data(){
@@ -109,38 +112,44 @@ export default {
       reg_alert_msg:"Please wait! Your account is being created."
     }
   },
+  computed:{
+    // ...mapActions(useUserStore,{createUser:"register"} )
+  },
   methods:{
+    ...mapActions(useUserStore,{createUser:"register"}),
     async registerForm(values){
       this.reg_show_alert=true;
       this.reg_in_submission=true;
       this.reg_alert_variant="bg-blue-500";
       this.reg_alert_msg="Please wait! Your account is being created.";
-      let userCred=null;
+      // let userCred=null;
       try {
-       userCred= await auth.createUserWithEmailAndPassword(
-         values.email, values.password
-       )
-      }catch (error){
+       // userCred= await auth.createUserWithEmailAndPassword(
+       //   values.email, values.password
+       // )
+        await this.createUser(values)
+      }catch(error){
         this.reg_in_submission=false;
         this.reg_alert_variant="bg-red-500";
         this.reg_alert_msg="An unexpected error occurred. Please try again later."
         // console.log(error)
         return;
       }
-      try{
-        await usersCollection.add({
-          name:values.name,
-          email:values.email,
-          age:values.age,
-          country:values.country,
-        });
-      }catch (error){
-        this.reg_in_submission=false;
-        this.reg_alert_variant="bg-red-500";
-        this.reg_alert_msg="An unexpected error occurred. Please try again later.";
-        console.log(error);
-        return;
-      }
+      // try{
+      //   await usersCollection.add({
+      //     name:values.name,
+      //     email:values.email,
+      //     age:values.age,
+      //     country:values.country,
+      //   });
+      // }catch (error){
+      //   this.reg_in_submission=false;
+      //   this.reg_alert_variant="bg-red-500";
+      //   this.reg_alert_msg="An unexpected error occurred. Please try again later.";
+      //   console.log(error);
+      //   return;
+      // }
+      // this.userStore.userLogin=true;
       this.reg_alert_variant="bg-green-500";
       this.reg_alert_msg="Success! Your account has been created."
       // console.log(userCred);
