44c8381307df2ad12e90012528d7f959aa087521
Juel-11001 <mdjewel20172017@gmail.com>
2025-07-06 02:36:54 +0600

    catch-all routes and redirect routes

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import Error404 from '@/views/Errors/404.vue'
 const routes=[
   {
     name:'home',
@@ -17,7 +18,16 @@ const routes=[
     name:'manage',
     path: '/manage',
     component: Manage,
+  },
+  {
+    path: '/manage-music',
+    redirect:{name:'manage'}
+  },
+  {
+    path:'/:catchAll(.*)*',
+    component: Error404
   }
+
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ music/src/views/Errors/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+      <h1 class="text-center font-bold text-red-400 text-3xl">404! Page not Found!</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
